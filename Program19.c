676958fd997b649dffe3d54f8ff8292631aacb75
anantmahadik <107344574+anantmahadik@users.noreply.github.com>
2023-03-24 19:30:55 +0530

    Add files via upload

---
+++ Program19.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;      //  X
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn->next = *First;
+        (*First)->prev = newn;  //  X
+
+        *First = newn;
+    }
+}
+
+void InsertLast(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE temp = *First;
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;      //  X
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+        temp->prev = temp;  //  X
+    }
+}
+
+int main()
+{
+    PNODE Head = NULL;
+
+    return 0;
+}
---
+++ Program22.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+    struct node *prev;
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;      //  X
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        newn->next = *First;
+        (*First)->prev = newn;  //  X
+
+        *First = newn;
+    }
+}
+
+void InsertLast(PPNODE First, int no)
+{
+    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE temp = *First;
+
+    newn->data = no;
+    newn->next = NULL;
+    newn->prev = NULL;      //  X
+
+    if(*First == NULL)
+    {
+        *First = newn;
+    }
+    else
+    {
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newn;
+        temp->prev = temp;  //  X
+    }
+}
+
+void Display(PNODE First)
+{
+    printf("Elements of Linked List are \n");
+
+    while(First != NULL)
+    {
+        printf(" | %d |->",First->data);
+        First = First->next;
+    }
+}
+
+int Count(PNODE First)
+{
+    int iCnt = 0;
+
+    while(First != NULL)
+    {
+        iCnt++;
+        First = First->next;
+    }
+    return iCnt;
+}
+
+void DeleteFirst(PPNODE First)
+{
+    if(*First == NULL)
+    {
+        return;
+    }
+    else if((*First)->next = NULL)
+    {
+        free(*First);
+        *First = NULL;
+    }
+    else
+    {
+        *First = (*First)->next;
+        free((*First)->prev);       // X
+        (*First)->prev = NULL;      // X
+    }
+}
+
+void DeleteLast(PPNODE First)
+{
+    PNODE temp = *First;
+
+    if(*First == NULL)
+    {
+        return;
+    }
+    else if((*First)->next == NULL)
+    {
+        free(*First);
+        *First = NULL;
+    }
+    else
+    {
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+        free(temp->next);
+        temp->next = NULL;
+    }
+}
+
+int main()
+{
+    PNODE Head = NULL;
+
+    return 0;
+}
